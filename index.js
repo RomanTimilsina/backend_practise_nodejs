92ed84abe83cad94198e4d682308d38365eba4af
Roman <romantimilsina@gmail.com>
2023-06-28 19:23:19 +0545

    upload_file_in_db_and_server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import  express  from "express"
 import  cors  from "cors"
+import  multer  from "multer"
+
 import  cookieParser  from "cookie-parser"
 
 const app = express()
@@ -10,11 +12,33 @@ import likeRoutes from './routes/likes.js'
 import commentRoutes from './routes/comments.js'
 import postRoutes from './routes/posts.js'
 
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true)
+  next()
+})
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin:"http://localhost:3000"
+  })
+);
 app.use(cookieParser())
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename );
+});
 
 
 app.use('/api/users', userRoutes)
@@ -27,3 +51,6 @@ app.use('/api/posts', postRoutes)
 app.listen(8800, () => {
   console.log("API working!")
 })
+
+
+
