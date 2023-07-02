cfe7dcd4904d9540ed18a153ef839d1fa4318ca1
Roman <romantimilsina@gmail.com>
2023-07-02 13:43:05 +0545

    fetch_profile_info

--- controllers/like.js
+++ controllers/like.js
@@ -10,5 +10,50 @@ export const getLikes = (req, res) => {
   });
 };
 
+export const addLikes = (req,res) => {
 
+  const token = req.cookies.accessToken;
+
+  if(!token) return res.status(401).json("Not logged in!")
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if(err) return res.status(401).json("Token is not valid!")
+
+    const q = "INSERT INTO likes (`userId`, `postId` ) VALUES (?)"
+
+    const values = [
+      userInfo.id,
+      req.body.postId
+    ]
+
+  db.query(q, [values],(err, data) => {
+    if (err) return res.status(500).json(err)
+    return res.status(200).json("Liked")
+  })
+  })
+}
+
+export const deleteLikes = (req,res) => {
+  const token = req.cookies.accessToken;
+
+  if(!token) return res.status(401).json("Not logged in!")
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if(err) return res.status(401).json("Token is not valid!")
+
+    const q = "DELETE FROM likes WHERE `userId` = ? AND `postId` = ?";
+    const values = [userInfo.id,req.query.postId]
+     
+  db.query(q, values,(err, data) => {
+    if (err) return res.status(500).json(err)
+    return res.status(200).json("disliked")
+  })
+  })
+  /* db.query(q, values, (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json("Post has been disliked.");
+  });
+}); */
+
+}
 
--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,14 @@
-export const getUser = () => {
-  
+import moment from "moment/moment.js";
+import { db } from "../connect.js"
+import jwt from 'jsonwebtoken'
+
+export const getUser = (req, res) => {
+  const userId = req.params.userId;
+  const q = 'SELECT * FROM users WHERE id = ?'
+
+  db.query(q, [userId], (err,data) => {
+    if (err) return res.json(err)
+    const {password, ...info} = data[0]
+    return res.json(info)
+  })
 }
